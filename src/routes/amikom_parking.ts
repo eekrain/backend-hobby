7900115bb1c3d837c06b4cb820bafd4f9f0ffb44
Ardian Eka Candra <ardianoption@gmail.com>
2023-05-21 20:32:09 +0700

    add web authentication endpoint

--- src/routes/amikom_parking.ts
+++ src/routes/amikom_parking.ts
@@ -59,4 +59,31 @@ AmikomParking.get('/user/listUser', async (req, res: Express.Response) => {
   res.status(200).send(users);
 });
 
+AmikomParking.post(
+  '/web/auth',
+  async (
+    req: TypedRequestBody<{ username: string; password: string }>,
+    res
+  ) => {
+    const { username, password } = req.body;
+    if (typeof username !== 'string' && typeof password !== 'string') {
+      return MY_ERRORS.BAD_REQUEST_400(res);
+    }
+
+    const passwordCorrect =
+      username === process.env.WEB_ADMIN_USERNAME &&
+      password === process.env.WEB_ADMIN_PASSWORD;
+
+    if (passwordCorrect) {
+      return res.status(200).send({
+        status: true,
+        message: 'Successfully Log In!'
+      });
+    }
+
+    console.error('Log In failed, wrong password');
+    return MY_ERRORS.UNAUTHORIZED_401(res);
+  }
+);
+
 export default AmikomParking;
--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -2,21 +2,16 @@ import type Express from 'express';
 
 const UNAUTHORIZED_401 = (
   res: Express.Response,
-  payload = {
-    status: false,
-    message: 'Gagal Log In!'
-  }
+  message = 'Credentials not found!'
 ) => {
-  return res.status(401).send(payload);
+  return res.status(401).send(message);
 };
 
 const BAD_REQUEST_400 = (
   res: Express.Response,
-  payload = {
-    message: 'Request information sent is not accepted'
-  }
+  message = 'Request information sent is not accepted!'
 ) => {
-  return res.status(400).send(payload);
+  return res.status(400).send(message);
 };
 
 const MY_ERRORS = { UNAUTHORIZED_401, BAD_REQUEST_400 };
