9ef539197b13e9d574f72a84b5723171325f1036
Ardian Eka Candra <ardianoption@gmail.com>
2023-05-30 16:11:12 +0700

    using argon2 instead of argon2-ffi

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,4 +1,4 @@
-import { argon2i } from 'argon2-ffi';
+import argon2 from 'argon2';
 import { randomBytes } from 'crypto';
 import { promisify } from 'node:util';
 
@@ -6,14 +6,14 @@ const getRandomBytes = promisify(randomBytes);
 
 export const generatePassword = async (password: string) => {
   const salt = await getRandomBytes(32);
-  const hashedPassword = await argon2i.hash(password, salt);
+  const hashedPassword = await argon2.hash(password, {});
   return hashedPassword;
 };
 
 export const verifyPassword = async (
   hashedPassword: string,
-  inputPassword: string
+  inputPassword: string,
 ) => {
-  const isCorrect = await argon2i.verify(hashedPassword, inputPassword);
+  const isCorrect = await argon2.verify(hashedPassword, inputPassword);
   return isCorrect;
 };
